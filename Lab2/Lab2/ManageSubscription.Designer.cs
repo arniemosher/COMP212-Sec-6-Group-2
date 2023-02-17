969490924875c57939a5632f6434ea918c5aac5b
arniemosher <arnabc768@yahoo.com>
2023-02-17 17:12:22 -0500

    cancel/exit button in manage sub

--- Lab2/Lab2/ManageSubscription.Designer.cs
+++ Lab2/Lab2/ManageSubscription.Designer.cs
@@ -41,48 +41,48 @@
             // chkSendEmail
             // 
             this.chkSendEmail.AutoSize = true;
-            this.chkSendEmail.Location = new System.Drawing.Point(83, 59);
-            this.chkSendEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkSendEmail.Location = new System.Drawing.Point(111, 73);
+            this.chkSendEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chkSendEmail.Name = "chkSendEmail";
-            this.chkSendEmail.Size = new System.Drawing.Size(147, 17);
+            this.chkSendEmail.Size = new System.Drawing.Size(182, 20);
             this.chkSendEmail.TabIndex = 0;
             this.chkSendEmail.Text = "Notification sent by E-mail";
             this.chkSendEmail.UseVisualStyleBackColor = true;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(242, 59);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtEmail.Location = new System.Drawing.Point(323, 73);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(150, 20);
+            this.txtEmail.Size = new System.Drawing.Size(199, 22);
             this.txtEmail.TabIndex = 1;
             this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
             // 
             // chkTextNotification
             // 
             this.chkTextNotification.AutoSize = true;
-            this.chkTextNotification.Location = new System.Drawing.Point(83, 109);
-            this.chkTextNotification.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkTextNotification.Location = new System.Drawing.Point(111, 134);
+            this.chkTextNotification.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chkTextNotification.Name = "chkTextNotification";
-            this.chkTextNotification.Size = new System.Drawing.Size(117, 17);
+            this.chkTextNotification.Size = new System.Drawing.Size(142, 20);
             this.chkTextNotification.TabIndex = 2;
             this.chkTextNotification.Text = "Notification by Text";
             this.chkTextNotification.UseVisualStyleBackColor = true;
             // 
             // txtTextNotification
             // 
-            this.txtTextNotification.Location = new System.Drawing.Point(242, 106);
-            this.txtTextNotification.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTextNotification.Location = new System.Drawing.Point(323, 130);
+            this.txtTextNotification.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTextNotification.Name = "txtTextNotification";
-            this.txtTextNotification.Size = new System.Drawing.Size(150, 20);
+            this.txtTextNotification.Size = new System.Drawing.Size(199, 22);
             this.txtTextNotification.TabIndex = 3;
             // 
             // btnSubscribe
             // 
-            this.btnSubscribe.Location = new System.Drawing.Point(74, 199);
-            this.btnSubscribe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSubscribe.Location = new System.Drawing.Point(99, 245);
+            this.btnSubscribe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSubscribe.Name = "btnSubscribe";
-            this.btnSubscribe.Size = new System.Drawing.Size(108, 36);
+            this.btnSubscribe.Size = new System.Drawing.Size(144, 44);
             this.btnSubscribe.TabIndex = 4;
             this.btnSubscribe.Text = "Subscribe";
             this.btnSubscribe.UseVisualStyleBackColor = true;
@@ -90,10 +90,10 @@
             // 
             // btnUnsubscribe
             // 
-            this.btnUnsubscribe.Location = new System.Drawing.Point(197, 199);
-            this.btnUnsubscribe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUnsubscribe.Location = new System.Drawing.Point(263, 245);
+            this.btnUnsubscribe.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUnsubscribe.Name = "btnUnsubscribe";
-            this.btnUnsubscribe.Size = new System.Drawing.Size(98, 36);
+            this.btnUnsubscribe.Size = new System.Drawing.Size(131, 44);
             this.btnUnsubscribe.TabIndex = 5;
             this.btnUnsubscribe.Text = "Unsubscribe";
             this.btnUnsubscribe.UseVisualStyleBackColor = true;
@@ -101,12 +101,12 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(318, 199);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCancel.Location = new System.Drawing.Point(424, 245);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(98, 36);
+            this.btnCancel.Size = new System.Drawing.Size(131, 44);
             this.btnCancel.TabIndex = 6;
-            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Text = "Cancel/Exit";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
@@ -114,18 +114,17 @@
             // 
             this.lblInvalidEmail.AutoSize = true;
             this.lblInvalidEmail.ForeColor = System.Drawing.Color.Firebrick;
-            this.lblInvalidEmail.Location = new System.Drawing.Point(240, 80);
-            this.lblInvalidEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInvalidEmail.Location = new System.Drawing.Point(320, 98);
             this.lblInvalidEmail.Name = "lblInvalidEmail";
-            this.lblInvalidEmail.Size = new System.Drawing.Size(66, 13);
+            this.lblInvalidEmail.Size = new System.Drawing.Size(83, 16);
             this.lblInvalidEmail.TabIndex = 7;
             this.lblInvalidEmail.Text = "Invalid Email";
             // 
             // ManageSubscription
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblInvalidEmail);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnUnsubscribe);
@@ -134,7 +133,7 @@
             this.Controls.Add(this.chkTextNotification);
             this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.chkSendEmail);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ManageSubscription";
             this.Text = "ManageSubscription";
             this.ResumeLayout(false);
